875fb421df4892ee0e9df3521b6b00d4871a2548
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-07-22 00:20:07 +0300

    added attach

--- src/test/java/PracticeFormRemoteTest.java
+++ src/test/java/PracticeFormRemoteTest.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,11 @@ public class PracticeFormRemoteTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void  addAttachments() {
+        Attach.screenshotAs("Last screenshot");
     }
 
     @Test

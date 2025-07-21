a35c7d9599586ef80b6878a8a48dad3d618afb81
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-07-22 00:16:42 +0300

    added demoqa test

--- src/test/java/PracticeFormRemoteTest.java
+++ src/test/java/PracticeFormRemoteTest.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,7 @@ public class PracticeFormRemoteTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @Test
